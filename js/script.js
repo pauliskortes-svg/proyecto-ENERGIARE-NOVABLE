72a9c345126bbe4b9c38dacf91babd23b197b8d4
paula cortes <pauliskortes@gmail.com>
2026-03-24 22:46:43 -0500

    navbar paula-footer y curiosidadesv1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
 // JS base del proyecto
 
 console.log("JS conectado correctamente");
+
+document.addEventListener("DOMContentLoaded",() => { 
+    const buttons=
+document.querySelectorAll(".accordion-btn");
+    buttons.forEach(button => { 
+button.addEventListener("click",() => { 
+    const content=
+button.nextElementSibling;
+content.classList.toggle("active");
+
+            });
+
+        });
+
+});
+
+
+
+
+
+
+
+
+
+
+
