51a782a3387b7076c7ec60314588cb39f71b3632
injrodriguezde <injrodriguezde@gmail.com>
2026-03-24 19:49:12 -0500

    Delete bucle.js

--- bucle.js
+++
@@ -1,8 +0,0 @@
-let numeros = "numeros del 1 al 100 <br>";
-for (let i = 1; i <= 100; i++)
-     {
-          numero +="numero: "
-     document.getElementById("resultado").innerHTML += i + "<br>";
-    }
-
-
